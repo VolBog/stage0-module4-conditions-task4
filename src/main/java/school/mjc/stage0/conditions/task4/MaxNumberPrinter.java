a4391c30c217cc64a0b187268e32a85d152d338e
volbog <volbog93@gmail.com>
2023-05-14 13:33:05 +0200

    finish all tasks

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first;
+        if (result < second) {
+            result = second;
+        }
+        if (result < third) {
+            result = third;
+        }
+        System.out.println(result);
     }
 }
